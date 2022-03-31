cf71c63e1b217ac89f46e1ff94fe726dfdb29a44
ewdmund <arefsajjadi80@gmail.com>
2022-04-01 03:06:03 +0430

    fix text truncate direction inside company header card

--- resources/views/layouts/companies/item/card.blade.php
+++ resources/views/layouts/companies/item/card.blade.php
@@ -1,6 +1,6 @@
 <div class="card shadow-sm" style="color: #9700ff; background-color: #f8fafc5e; border-color: #a402ff;">
     <div class="card-body">
-        <h5 class="card-title text-center text-truncate">
+        <h5 class="card-title text-center text-truncate" dir="rtl">
             <a href="{{route('companies.show', [$company->id])}}" target="_blank" class="link-primary">{{$company->name}}</a>
         </h5>
     </div>
