e043da8ebd3efa4c5dc29390ea7af3af97399dd9
ewdmund <arefsajjadi80@gmail.com>
2022-05-13 00:28:32 +0430

    fix comapny card

--- resources/views/layouts/companies/item/card.blade.php
+++ resources/views/layouts/companies/item/card.blade.php
@@ -2,15 +2,17 @@
     <div>
         <div class="card-body">
             <h5 class="card-title text-center text-truncate" dir="rtl">
-                <a href="{{route('companies.show', [$company->id])}}" target="_blank" class="link-primary">{{$company->name}}</a>
+                <a href="{{route('companies.show', [$company->id])}}" target="_blank"
+                   class="link-primary">{{$company->name}}</a>
             </h5>
         </div>
 
         <div class="text-center">
             @include('layouts.companies.Indicators')
         </div>
-
-        <div class="card-body btn-group btn-group-sm">
+    </div>
+    <div class="mt-auto">
+        <div class="card-body btn-group btn-group-sm w-100">
             <a class="card-link btn btn-outline-secondary"
                href="{{route('comments.create', $company->id)}}">ثبت تجربه</a>
             <a class="card-link btn btn-outline-secondary"
